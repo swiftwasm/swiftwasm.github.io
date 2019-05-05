12d78b78278a52fd146dbbc6ffb92ac4b6e1f8d8
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-05-05 11:16:50 -0700

    Simplify demo script

--- home.js
+++ home.js
@@ -110,8 +110,6 @@ function downloadWasmClicked(output) {
 // Demo script
 const kDefaultDemoScript = `import Glibc
 
-// Print statements work
-
 print("Hello, 🌐!")
 
 // we can try loops and arithmetic:
@@ -133,46 +131,33 @@ func fizzBuzz(from: Int, to: Int) {
 fizzBuzz(from: 1, to: 20)
 
 func fib(n: Int) -> Int {
-    if n <= 0 {
-        return 0;
-    }
-    if n <= 2 {
-        return 1;
-    }
+    if n <= 0 { return 0 }
+    if n <= 2 { return 1 }
     var a = 1;
     var b = 1;
     for _ in 3...n {
-        let newA = b;
-        let newB = a + b;
+        let newA = b
+        let newB = a + b
         a = newA
         b = newB
     }
     return b
 }
 
-print("The 10th fibonacci number is \(fib(n: 10))")
+print("The 10th Fibonacci number is \(fib(n: 10))")
 
 // we can also run JavaScript from Swift.
-// Here's a string holding JavaScript code, with some string interpolation:
-
-let scriptSrc = """
-    const div = document.createElement("div");
-    div.textContent = "Hello from Swift! The 10th Fibonacci number is \(fib(n: 10))!";
-    document.body.appendChild(div);
-    alert("I'm called from Swift!");
-"""
 
 func executeScript(script: String) {
-    // WASI doesn't specify a standard way to invoke JavaScript yet,
-    // so this is a kludge.
     let magicFd:Int32 = -1337
     if write(magicFd, script, strlen(script)) == -1 {
-    print("Can't execute script on this runtime: please use the SwiftWasm polyfill on https://swiftwasm.org.")
-        print("The script is:")
+        print("Can't execute script: please use the SwiftWasm polyfill on https://swiftwasm.org.")
         print(script)
     }
 }
 
+// Here's a string holding JavaScript code, with some string interpolation:
+let scriptSrc = "alert('The 11th Fibonacci number is \(fib(n: 11))');"
 // and we can execute it.
 executeScript(script: scriptSrc)
 `;
