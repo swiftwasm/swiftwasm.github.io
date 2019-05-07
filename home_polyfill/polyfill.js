a94d7f8956e05f344994ba757480588a3e641563
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-05-06 21:16:13 -0700

    Update polyfill to report errors better

--- home_polyfill/polyfill.js
+++ home_polyfill/polyfill.js
@@ -1732,11 +1732,11 @@ function updateGlobalBufferViews() {
 
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 6016,
+    STACK_BASE = 6096,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 5248896,
-    DYNAMIC_BASE = 5248896,
-    DYNAMICTOP_PTR = 5760;
+    STACK_MAX = 5248976,
+    DYNAMIC_BASE = 5248976,
+    DYNAMICTOP_PTR = 5840;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 assert(DYNAMIC_BASE % 16 === 0, 'heap must start aligned');
@@ -2202,7 +2202,7 @@ Module['asm'] = function(global, env, providedBuffer) {
 
 // === Body ===
 
-var ASM_CONSTS = [function() { const imports = { wasi_unstable: WASIPolyfill };         WebAssembly.instantiate(wasi_wasm_buffer, imports)         .then(obj => {             setInstance(obj.instance);             try {                 obj.instance.exports._start();             } catch (e) {                 if (e instanceof WASIExit) {                     handleWASIExit(e);                 } else {                     console.log(e);                 }             }         })         .catch(error => {             console.log('error! ' + error);         }); }];
+var ASM_CONSTS = [function() { const imports = { wasi_unstable: WASIPolyfill };         WebAssembly.instantiate(wasi_wasm_buffer, imports)         .then(obj => {             setInstance(obj.instance);             try {                 obj.instance.exports._start();             } catch (e) {                 if (e instanceof WASIExit) {                     handleWASIExit(e);                 } else {                     console.log(e);                     if (window.wasi_handle_error) window.wasi_handle_error(e);                 }             }         })         .catch(error => {             console.log('error! ' + error);         }); }];
 
 function _emscripten_asm_const_i(code) {
   return ASM_CONSTS[code]();
@@ -2211,7 +2211,7 @@ function _emscripten_asm_const_i(code) {
 
 
 
-// STATICTOP = STATIC_BASE + 4992;
+// STATICTOP = STATIC_BASE + 5072;
 /* global initializers */ /*__ATINIT__.push();*/
 
 
@@ -2222,7 +2222,7 @@ function _emscripten_asm_const_i(code) {
 
 
 /* no memory initializer */
-var tempDoublePtr = 6000
+var tempDoublePtr = 6080
 assert(tempDoublePtr % 8 == 0);
 
 function copyTempFloat(ptr) { // functions, because inlining this code increases code size too much
@@ -3525,11 +3525,11 @@ function copyTempDouble(ptr) {
   
   var ERRNO_CODES={EPERM:1,ENOENT:2,ESRCH:3,EINTR:4,EIO:5,ENXIO:6,E2BIG:7,ENOEXEC:8,EBADF:9,ECHILD:10,EAGAIN:11,EWOULDBLOCK:11,ENOMEM:12,EACCES:13,EFAULT:14,ENOTBLK:15,EBUSY:16,EEXIST:17,EXDEV:18,ENODEV:19,ENOTDIR:20,EISDIR:21,EINVAL:22,ENFILE:23,EMFILE:24,ENOTTY:25,ETXTBSY:26,EFBIG:27,ENOSPC:28,ESPIPE:29,EROFS:30,EMLINK:31,EPIPE:32,EDOM:33,ERANGE:34,ENOMSG:42,EIDRM:43,ECHRNG:44,EL2NSYNC:45,EL3HLT:46,EL3RST:47,ELNRNG:48,EUNATCH:49,ENOCSI:50,EL2HLT:51,EDEADLK:35,ENOLCK:37,EBADE:52,EBADR:53,EXFULL:54,ENOANO:55,EBADRQC:56,EBADSLT:57,EDEADLOCK:35,EBFONT:59,ENOSTR:60,ENODATA:61,ETIME:62,ENOSR:63,ENONET:64,ENOPKG:65,EREMOTE:66,ENOLINK:67,EADV:68,ESRMNT:69,ECOMM:70,EPROTO:71,EMULTIHOP:72,EDOTDOT:73,EBADMSG:74,ENOTUNIQ:76,EBADFD:77,EREMCHG:78,ELIBACC:79,ELIBBAD:80,ELIBSCN:81,ELIBMAX:82,ELIBEXEC:83,ENOSYS:38,ENOTEMPTY:39,ENAMETOOLONG:36,ELOOP:40,EOPNOTSUPP:95,EPFNOSUPPORT:96,ECONNRESET:104,ENOBUFS:105,EAFNOSUPPORT:97,EPROTOTYPE:91,ENOTSOCK:88,ENOPROTOOPT:92,ESHUTDOWN:108,ECONNREFUSED:111,EADDRINUSE:98,ECONNABORTED:103,ENETUNREACH:101,ENETDOWN:100,ETIMEDOUT:110,EHOSTDOWN:112,EHOSTUNREACH:113,EINPROGRESS:115,EALREADY:114,EDESTADDRREQ:89,EMSGSIZE:90,EPROTONOSUPPORT:93,ESOCKTNOSUPPORT:94,EADDRNOTAVAIL:99,ENETRESET:102,EISCONN:106,ENOTCONN:107,ETOOMANYREFS:109,EUSERS:87,EDQUOT:122,ESTALE:116,ENOTSUP:95,ENOMEDIUM:123,EILSEQ:84,EOVERFLOW:75,ECANCELED:125,ENOTRECOVERABLE:131,EOWNERDEAD:130,ESTRPIPE:86};
   
-  var _stdin=5776;
+  var _stdin=5856;
   
-  var _stdout=5792;
+  var _stdout=5872;
   
-  var _stderr=5808;var FS={root:null,mounts:[],devices:{},streams:[],nextInode:1,nameTable:null,currentPath:"/",initialized:false,ignorePermissions:true,trackingDelegate:{},tracking:{openFlags:{READ:1,WRITE:2}},ErrnoError:null,genericErrors:{},filesystems:null,syncFSRequests:0,handleFSError:function (e) {
+  var _stderr=5888;var FS={root:null,mounts:[],devices:{},streams:[],nextInode:1,nameTable:null,currentPath:"/",initialized:false,ignorePermissions:true,trackingDelegate:{},tracking:{openFlags:{READ:1,WRITE:2}},ErrnoError:null,genericErrors:{},filesystems:null,syncFSRequests:0,handleFSError:function (e) {
         if (!(e instanceof FS.ErrnoError)) throw e + ' : ' + stackTrace();
         return ___setErrNo(e.errno);
       },lookupPath:function (path, opts) {
